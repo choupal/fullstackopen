ed56c6eea1565e878c7ad5061b1b4a06efbce638
choupal <dongoisclement.dev@pm.me>
2022-10-25 11:45:16 +0200

    part1c-d

--- cours/part1/src/App.js
+++ cours/part1/src/App.js
@@ -1,20 +1,26 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello There {props.name}, you are {props.age} years old</p></div>
-  )
-}
+import { useState } from "react";
+
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const name = "Obi-Wan"
-  const age = 30
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="General" age={26+11} />
-      <Hello name={name} age={age}/>
-    </>
-  )
-}
+  const [value, setValue] = useState(10);
 
-export default App
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  );
+};
+export default App;
