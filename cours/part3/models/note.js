5e354e7e0b19b0d724978aeddbabcedef7a816ed
choupal <dongoisclement.dev@pm.me>
2022-11-17 13:48:07 +0100

    part4a

--- cours/part3/models/note.js
+++ cours/part3/models/note.js
@@ -1,30 +1,24 @@
-const mongoose = require("mongoose");
-
-const url = process.env.MONGODB_URL;
-
-console.log("connecting to MongoDB");
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: { type: String, minLength: 5, required: true },
-  date: { type: Date, required: true },
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
   },
-});
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ exercices/part4/blog/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const router = require('./controllers/router')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+logger.info('connecting to MongoDB')
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', router)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
